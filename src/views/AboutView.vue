ff41cb39a20e007a9ab026df1c0e7b88f1d7981e
David Garay <garaekz@gmail.com>
2024-02-24 23:33:08 -0800

    Show Skills

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -22,10 +22,60 @@ import ExperienceTimeline from '@/components/ExperienceTimeline.vue';
         and other technologies, driven by my love for learning and continuous improvement.
       </p>
     </main>
+    <section id="skills" class="w-full flex flex-col mt-20">
+      <div class="w-full flex flex-col my-12">
+        <h2 class="text-6xl text-emerald-600 dark:text-emerald-300 font-bold">Skills<span
+            class="text-7xl text-zinc-400 dark:text-gray-300 font-bold">.</span></h2>
+        <p class="text-2xl mt-4 mb-8">
+          What can I do and using what<span class="text-3xl text-emerald-600 dark:text-emerald-400">?</span>  
+        </p>
+        <div class="w-full grid grid-cols-2 lg:grid-cols-4 gap-4 mt-8">
+          <div>
+            <h3 class="text-4xl font-bold">Languages</h3>
+            <ul class="text-2xl mt-4">
+              <li>PHP</li>
+              <li>TypeScript</li>
+              <li>Go</li>
+              <li>Python <span class="text-sm text-emerald-400">(Basic)</span></li>
+              <li>Ruby <span class="text-sm text-emerald-400">(Basic)</span></li>
+            </ul>
+          </div>
+          <div>
+            <h3 class="text-4xl font-bold">Frameworks</h3>
+            <ul class="text-2xl mt-4">
+              <li>Laravel</li>
+              <li>Vue.js</li>
+              <li>TailwindCSS</li>
+              <li>React</li>
+              <li>Ruby on Rails <span class="text-sm text-emerald-400">(Basic)</span></li>
+            </ul>
+          </div>
+          <div>
+            <h3 class="text-4xl font-bold">Databases</h3>
+            <ul class="text-2xl mt-4">
+              <li>MySQL</li>
+              <li>PostgreSQL</li>
+              <li>MongoDB</li>
+              <li>Redis</li>
+            </ul>
+          </div>
+          <div>
+            <h3 class="text-4xl font-bold">Soft Skills</h3>
+            <ul class="text-2xl mt-4">
+              <li>Problem Solving</li>
+              <li>Teamwork</li>
+              <li>Communication</li>
+              <li>English Proficient <span class="text-emerald-400">(C2)</span></li>
+              <li>Leadership</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
     <section id="experience" class="w-full flex flex-col">
       <div class="w-full flex flex-col my-12">
-        <h2 class="text-6xl text-emerald-600 dark:text-emerald-300 font-bold">Experience<span
-            class="text-7xl text-zinc-400 dark:text-gray-300 font-bold">.</span></h2>
+        <h2 class="text-6xl font-bold">Experience<span
+            class="text-7xl text-emerald-400">.</span></h2>
       </div>
       <experience-timeline />
     </section>
